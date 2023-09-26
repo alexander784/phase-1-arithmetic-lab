031476215e0ea7b773916736ea78c42b741e1885
alexander <alexandernyaga1999@gmail.com>
2023-09-26 12:09:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+const random = Math.floor(Math.random() *Number.MAX_SAFE_INTEGER) +1;
+
+console.log(random);
+
+const num3 = 12;
+const num4 = 3;
+
+const mod = num3/num4;
+
+console.log(mod);
+
+
+var numbers = [2, 10, 20, 12];
+
+var max = Math.max(...numbers);
+
+console.log(max); 
+
